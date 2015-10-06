9e74e913800b39988fc62c45c9dca0c6db6d54a0
Roman <ritsarkov@gmail.com>
2015-10-06 15:24:25 +0300

    Initial commit

---
+++ src/com/example/MyTestApplication/TestApplicationActivity.java
@@ -0,0 +1,15 @@
+package com.example.MyTestApplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestApplicationActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
